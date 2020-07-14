f88e949fdb43a8019058862fe3637e5204bc88b4
haytt.xiang <haytt.xiang@xgimi.com>
2020-07-14 20:06:15 +0800

    springmvc-simplechangge

--- mvc-data/src/main/java/com/xxt/springmvc/componet/MySimpleConverter.java
+++ mvc-data/src/main/java/com/xxt/springmvc/componet/MySimpleConverter.java
@@ -5,7 +5,7 @@ import org.springframework.core.convert.converter.Converter;
 public class MySimpleConverter implements Converter<String,String> {
     @Override
     public String convert(String s) {
-        System.out.println("I'm ok");
+        System.out.println("have changed");
         return null;
     }
 }
